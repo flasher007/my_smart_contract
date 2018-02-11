1e499649f4a60d9c1905a7c07ce2fd3f47e3e9ea
Aleksandr Volkov <avolkov05@gmail.com>
2018-02-11 21:03:53 +0700

    add token contract

---
+++ contracts/AAVToken.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.11;
+import "zeppelin-solidity/contracts/token/StandardToken.sol";
+
+contract AAVToken {
+
+    string public constant name = "Aleksandr Volkov Token Test";
+    string public constant symbol = "AAVT";
+    uint8 public constant decimals = 18;
+
+    uint public totalSupply = 31000000 * (10 ** decimals);
+
+    function AAVToken(){
+        balances[msg.sender] = totalSupply;
+    }
+}
+
