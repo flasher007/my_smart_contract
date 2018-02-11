7b8797369f8867555095ca134e010b7b792e984e
Aleksandr Volkov <avolkov05@gmail.com>
2018-02-12 01:05:54 +0700

    add tokens

--- contracts/AAVToken.sol
+++ contracts/AAVToken.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.11;
-import "zeppelin-solidity/contracts/token/StandardToken.sol";
+import "zeppelin-solidity/contracts/token/MintableToken.sol";
 
-contract AAVToken {
+contract AAVToken is MintableToken {
 
     string public constant name = "Aleksandr Volkov Token Test";
     string public constant symbol = "AAVT";
---
+++ contracts/AAVTokenICO.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.4.11;
+import "zeppelin-solidity/contracts/math/SafeMath.sol";
+interface AAVTokenInterface {
+    function transfer(address _receiver, uint256 _amount);
+}
+
+contract AAVICOContract {
+    using SafeMath for uint256;
+    uint public buyPrice;
+
+    AAVTokenInterface public token;
+
+    function AAVICOContract(AAVTokenInterface _token){
+        token = _token;
+        buyPrice = 1000000;
+    }
+
+    function () payable {
+        _buy(msg.sender, msg.value);
+    }
+
+    function buy() payable returns (uint) {
+        uint tokens = _buy(msg.sender, msg.value);
+        return tokens;
+    }
+
+    function _buy(address _sender, uint256 _amount) internal returns (uint){
+        uint tokens = div(_amount, buyPrice);
+        token.transfer(_sender, tokens);
+        return tokens;
+    }
+}
