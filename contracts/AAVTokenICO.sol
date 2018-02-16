209677357ff3c7203b909c7daefcad5ee1f0d660
Aleksandr Volkov <avolkov05@gmail.com>
2018-02-16 14:07:26 +0700

    add oraclizer

--- contracts/AAVTokenICO.sol
+++ contracts/AAVTokenICO.sol
@@ -1,19 +1,30 @@
 pragma solidity ^0.4.18;
 import "zeppelin-solidity/contracts/math/SafeMath.sol";
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
 
 interface AAVTokenInterface {
     function transfer(address _receiver, uint256 _amount) public;
 }
 
-contract AAVICOContract {
-    using SafeMath for uint256;
-    uint256 public buyPrice;
+contract AAVICOContract is usingOraclize, Ownable {
+    using SafeMath for uint;
+    uint public constant usdRate = 100; //in cents
+    uint public buyPrice;
+    uint public ETHUSDPrice;
+    uint public updateInterval = 43200; //12 hours by default
+    string public url = "json(https://api.kraken.com/0/public/Ticker?pair=ETHUSD).result.XETHZUSD.c.[0]";
+
+    event newOraclizeQuery(string description);
+    event newPriceTicker(uint price);
 
     AAVTokenInterface public token;
 
     function AAVICOContract(AAVTokenInterface _token) public {
         token = _token;
-        buyPrice = 1000000;
+
+        oraclize_setProof(proofType_TLSNotary | proofStorage_IPFS);
+        update();
     }
 
     function () public payable {
@@ -26,8 +37,30 @@ contract AAVICOContract {
     }
 
     function _buy(address _sender, uint256 _amount) internal returns (uint){
-        uint tokens = div(_amount, buyPrice);
+        uint tokens = _amount.mul(ETHUSDPrice).div(usdRate);
         token.transfer(_sender, tokens);
         return tokens;
     }
+
+    function __callback(bytes32 myid, string result, bytes proof) {
+        if (msg.sender != oraclize_cbAddress()) throw;
+        ETHUSDPrice = parseInt(result, 2);
+        newPriceTicker(ETHUSDPrice);
+        update();
+    }
+
+    function update() payable {
+        if (oraclize_getPrice("URL") > this.balance) {
+            newOraclizeQuery("Oraclize query was NOT sent, please add some ETH to cover for the query fee");
+        } else {
+            newOraclizeQuery("Oraclize query was sent, standing by for the answer..");
+            oraclize_query(updateInterval, "URL", url);
+        }
+    }
+
+    function setUpdateInterval(uint newInterval) external onlyOwner {
+        require(newInterval > 0);
+        updateInterval = newInterval;
+    }
+
 }
